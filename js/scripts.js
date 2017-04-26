dfda787e6d301b5a5c37bf0567e20dad62e21508
John Doe <john.doe@example.com>
2017-04-26 19:19:52 +0200

    scripts.js modified2

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,13 @@
+function getTriangleArea(a, h) {
 
-    function getTriangleArea(a, h) {
-
-		// Instrukcje warunkowe
-		if (a < 0 || h < 0) {
-			return console.log('Nieprawidłowe dane');
-		} else { return a*h/2 }
-	}
+	// Instrukcje warunkowe
+	if (a < 0 || h < 0) {
+		return console.log('Nieprawidłowe dane');
+	} else { return a*h/2 }
+}
 
 // Wyświetlanie wyniku funkcji
-console.log( getTriangleArea(10,6) );
+console.log(getTriangleArea(10,6));
 
 var triangle1Area = getTriangleArea(10, 15);
 var triangle2Area = getTriangleArea(5, 5);
